5a68c2dfa07e4b763300db8d7cb13c078e53ea2c
rachael-pluta <rachael.pluta.work@gmail.com>
2022-09-11 14:30:53 +1000

    added current date to header in js file

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Displays current date
+var currentDate = moment().format("dddd, MMMM Do YYYY");
+$("#currentDay").html(currentDate);
+
+// Color codes timeblocks to past, present and future
+
+// Saves the event to local storage
